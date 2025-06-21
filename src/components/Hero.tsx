80e9fbc26ba4e4003235710f21035e6cb5dc0e4d
Endelif <92841501+idkwhyi@users.noreply.github.com>
2025-06-21 09:41:08 +0700

    Adding font configuration

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Hero = () => {
-    return <div className='text-matcha'>THIS IS HERO SECTION</div>
+    return (
+        <div className="text-matcha">
+            <h1 className="text-4xl font-bold mb-4">Browse Everythting</h1>
+        </div>
+    )
 }
 
 export default Hero

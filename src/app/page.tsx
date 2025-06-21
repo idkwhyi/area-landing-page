964637c79538b4b5d79de552676b459725f03956
Endelif <92841501+idkwhyi@users.noreply.github.com>
2025-06-21 09:29:56 +0700

    Update tailwind library, Adding custom color and test the custom color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen bg-blue-400">
+    <div className="items-center justify-items-center min-h-screen bg-white">
         <Navbar/>
-
-        <div className="w-40 h-40 bg-red-200">TEST</div>
+        <br/>
+        <Hero/>
+        <div className="w-40 h-40 bg-greenbackground">TEST</div>
         <div className="w-40 h-40 bg-red-200">TEST</div>
         <div className="w-40 h-40 bg-red-200">TEST</div>
         <div className="w-40 h-40 bg-red-200">TEST</div>
---
+++ src/components/Hero.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Hero = () => {
+    return <div className='text-matcha'>THIS IS HERO SECTION</div>
+}
+
+export default Hero
